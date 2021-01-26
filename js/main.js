5bb9f2ba4c7d4897f72dfb115a1caa5e5d5c43ca
nvalerio12 <nelson.j.valerio@gmail.com>
2021-01-25 19:17:10 -0500

    HTML/CSS/js all started

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelectorAll('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+
+    let currentPlayer = 1
+
+    for (var i = 0, len = squares.length; i < len; i++)
+
+    (function(index)) {
+        // add an onclick to each square on the grid
+    }
+
+})
