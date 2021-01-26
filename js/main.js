b54e51c72dc4dd74ba133f9cccceefab01a0cbbc
nvalerio12 <nelson.j.valerio@gmail.com>
2021-01-26 16:13:49 -0500

    finished board working on second html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const squares = document.querySelectorAll('.grid div')
-    const result = document.querySelectorAll('#result')
-    const displayCurrentPlayer = document.querySelector('#current-player')
+// Selectors
 
-    let currentPlayer = 1
+var tableRow = document.querySelectorAll('tr');
+var tableCell = document.querySelectorAll('td');
+var tableSlot = document.querySelector('.slot');
+const playerTurn = document.querySelector('.playerturn');
+const reset = document.querySelector('.reset');
 
-    for (var i = 0, len = squares.length; i < len; i++)
+for (let i = 0; 0 < tableCell.lengt; i++) {
+    tableCell[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
+    })
+}
 
-    (function(index)) {
-        // add an onclick to each square on the grid
-    }
+while (!player1) {
+    var player1 = prompt(`Player One: Enter Your Name. You will be RED.`);
+}
 
+player1Color = 'red'
+
+while (!player2) {
+    var player2 = prompt(`Player Two: Enter Your Name. You will be YELLOW.`);
+}
+
+player2Color = 'yellow'
+
+var currentPlayer = 1;
+playerTurn.textContent = `${player1}'s turn!`;
+
+Array.prototype.forEach.call(tableCell, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = 'white';
 })
+
+function changeColor(e) {
+    let column = e.target.cellIndex;
+    let row = [];
+
+    for (let i = 5; i > -1; i--) {
+        if (tableRow[i].children[column].style.backgroundColor == 'white') {
+            row.push(tableRow[i].children[column]);
+            if (currentPlayer === 1) {
+                row(0).style.backgroundColor = player1Color
+            }
+        }
+    }
+}
+
