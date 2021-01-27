691026b1dcb2eae997d54fd8f35ef66578508719
nvalerio12 <nelson.j.valerio@gmail.com>
2021-01-27 14:30:15 -0500

    started js need to start functions

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Project-1-for-GA-SEI
+
+Project Title: Connect 4
+
+Motivation: 
+Is a game that I will create from scratch and be able to play with family and friends
+
+Screenshots:
+N/A at the moment. Will have some soon.
+
+Technologies & Code Snippets: 
+HTML CSS Javascript
+
+Credits:
+To Howard Wexler's the creater of connect 4 amde something so simple fun. 
+
+Future development:
+
+3 different load up events
+- First would be player welcome screen
+- Would be able to choose who goes first
+- Whos turn it is will be displayed at bottom of screen
+- Winner screen With huge congraulations with the winner color in it 
+- pop up refresh/restart button 
--- js/main.js
+++ js/main.js
@@ -1,48 +1,34 @@
 // Selectors
 
-var tableRow = document.querySelectorAll('tr');
-var tableCell = document.querySelectorAll('td');
-var tableSlot = document.querySelector('.slot');
-const playerTurn = document.querySelector('.playerturn');
-const reset = document.querySelector('.reset');
+var tableRow = document.getElementsByTagName('tr')
+var tableCell = document.getElementsByTagName('td')
+var tableSlot = document.querySelector('.slot')
+const playerTurn = document.querySelector('.player-turn')
+const reset = document.querySelector('.rest')
 
-for (let i = 0; 0 < tableCell.lengt; i++) {
-    tableCell[i].addEventListener('click', (e) => {
-        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
-    })
-}
+for (i = 0; i < tableData.length; i ++){
+    tableData[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
 
-while (!player1) {
-    var player1 = prompt(`Player One: Enter Your Name. You will be RED.`);
-}
+// while (!player1) {
+//     var player1 = prompt('Player One: Enter your name. You will be red.');
+// };
+// var player1Color = 'red';
 
-player1Color = 'red'
-
-while (!player2) {
-    var player2 = prompt(`Player Two: Enter Your Name. You will be YELLOW.`);
-}
-
-player2Color = 'yellow'
+// while (!player2) {
+//     var player2 = prompt('Player Two: Enter your name. You will be yellow.');
+// };
+// var player2Color = 'yellow';
 
 var currentPlayer = 1;
-playerTurn.textContent = `${player1}'s turn!`;
+playerTurn.textContent = `${player1}'s turn!`
 
-Array.prototype.forEach.call(tableCell, (cell) => {
+Array.prototype.forEach.call(tableData, (cell) => {
     cell.addEventListener('click', changeColor);
+    // Set all slots to white for new game.
     cell.style.backgroundColor = 'white';
-})
+});
 
-function changeColor(e) {
-    let column = e.target.cellIndex;
-    let row = [];
-
-    for (let i = 5; i > -1; i--) {
-        if (tableRow[i].children[column].style.backgroundColor == 'white') {
-            row.push(tableRow[i].children[column]);
-            if (currentPlayer === 1) {
-                row(0).style.backgroundColor = player1Color
-            }
-        }
-    }
-}
-
+//Function
